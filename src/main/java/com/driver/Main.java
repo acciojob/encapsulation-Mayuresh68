e514bbd80de6cf6a5b6702b90470a5180b808188
Mayuresh <mayureshpatil068@gmail.com>
2023-02-12 10:35:32 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("mayuresh_patil");
+        System.out.println(obj.getName());
+
+        RWOnly obj1 = new RWOnly();
+        obj1.setName("lets try...");
+        System.out.println(obj1.getName());
+
+        obj.setNum(50);
+        System.out.println(obj.getNum());
+    }
 }
